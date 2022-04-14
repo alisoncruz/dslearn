4c93c8c05ff202264baa30d1764e3dd63c0b5449
alisoncruz <alisonn83@gmail.com>
2022-04-14 10:18:20 -0300

    Resource and Section

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -17,3 +17,12 @@ INSERT INTO tb_course(name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML','http
 
 INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z',1);
 INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z',1);
+
+INSERT INTO tb_resource(title, description, position, img_uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso',1,'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.hfPS1kCAGAFFcE-tlmpWMgHaDt%26pid%3DApi&f=1',1,1);
+INSERT INTO tb_resource(title, description, position, img_uri, type, offer_id) VALUES ('Forum', 'Tira suas dúvidas',2,'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.hfPS1kCAGAFFcE-tlmpWMgHaDt%26pid%3DApi&f=1',2,1);
+INSERT INTO tb_resource(title, description, position, img_uri, type, offer_id) VALUES ('Lives','Lives exclusivas para a turma',3,'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.hfPS1kCAGAFFcE-tlmpWMgHaDt%26pid%3DApi&f=1',0,1);
+
+INSERT INTO tb_section (title, description, position, img_uri,resource_id,prerequisite_id) VALUES ('Capítulo 1','Neste capítulo vamos começar',1,'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.hfPS1kCAGAFFcE-tlmpWMgHaDt%26pid%3DApi&f=1',1,null);
+INSERT INTO tb_section (title, description, position, img_uri,resource_id,prerequisite_id) VALUES ('Capítulo 2','Neste capítulo vamos começar',2,'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.hfPS1kCAGAFFcE-tlmpWMgHaDt%26pid%3DApi&f=1',1,1);
+INSERT INTO tb_section (title, description, position, img_uri,resource_id,prerequisite_id) VALUES ('Capítulo 3','Neste capítulo vamos começar',3,'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.hfPS1kCAGAFFcE-tlmpWMgHaDt%26pid%3DApi&f=1',1,2);
+
